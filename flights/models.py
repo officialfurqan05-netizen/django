5af3d64e2d1c2d48544447856ebf77773c0069e7
furqan156 <officialfurqan05@gmail.com>
2026-08-15 23:54:11 +0500

    testing

--- flights/models.py
+++ flights/models.py
@@ -10,6 +10,10 @@ class Flight(models.Model):
     origin=models.ForeignKey(Airport,on_delete=models.CASCADE,related_name="departures")
     destination=models.ForeignKey(Airport,on_delete=models.CASCADE,related_name="arrivals")
     duration=models.IntegerField()
+
+    def is_valid_flight(self):
+        return (self.origin != self.destination) and (self.duration >= 0)
+    
     def __str__(self):
         return f"{self.id} - {self.origin} to {self.destination} in {self.duration} min"
 class Passenger(models.Model):
@@ -17,4 +21,4 @@ class Passenger(models.Model):
     last = models.CharField(max_length=64)
     flights = models.ManyToManyField(Flight , blank=True , related_name="passengers")
     def __str__(self):
-        return f"{self.first} {self.last}"
+        return f"{self.first} {self.last}" 
